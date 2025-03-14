35ee11168272d910cb9d5fae41440512691768aa
baderdinedev <baderdinedev@gmail.com>
2025-03-14 15:22:37 +0700

    first commit

--- projetfs/Program.cs
+++ projetfs/Program.cs
@@ -11,7 +11,7 @@ namespace projetfs
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new FRM_Ajouter_Modifie());
+            Application.Run(new FRM_Menu());
         }
     }
 }
